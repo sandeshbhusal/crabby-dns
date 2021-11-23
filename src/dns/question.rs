e441bff70ff08d182c450df90fea41ae3298d72e
Sandesh Bhusal <sandeshbhusal@Sandeshs-MacBook-Pro.local>
2021-11-23 21:05:15 +0545

    Added support for Question parsing

---
+++ src/dns/question.rs
@@ -0,0 +1,79 @@
+#[derive(Debug)]
+enum TYPE {
+    A,
+    AAAA,
+    MX,
+    CNAME,
+    NS,
+
+    UNKNOWN
+}
+
+#[derive(Debug)]
+enum CLASS {
+    RESERVED,
+    IN,
+    CH,
+    HS,
+    NONE,
+
+    UNKNOWN
+}
+
+#[derive(Debug)]
+pub struct DNSQuestion {
+    _name: String,
+    _type: TYPE,
+    _class: CLASS
+}
+
+impl Default for DNSQuestion {
+    fn default() -> Self { 
+        return DNSQuestion {
+            _name: String::from(""),
+            _type: TYPE::UNKNOWN,
+            _class: CLASS::UNKNOWN
+        }
+    }
+}
+
+impl DNSQuestion{
+    pub fn new(bytestream: &[u8]) -> Result<Self, Box<dyn std::error::Error>> {
+        let mut _name = String::new();
+        let mut _i = 0usize; // index for the bytestream
+
+        while bytestream[_i] != 0 {
+            // Read number of bytes from _i. 
+            let mut _readable = bytestream[_i];
+            _i += 1;
+            while _readable > 0 {
+                _name.push(bytestream[_i] as char);
+                _i += 1;
+                _readable -= 1;
+            }
+
+            _name.push('.');
+        }
+
+        _i += 1; // Skip the null termination character. 
+        
+        let _type = match (bytestream[_i] as u16) << 8 | bytestream[_i + 1] as u16 {
+            1 => TYPE::A,
+            5 => TYPE::NS,
+            _ => TYPE::UNKNOWN
+        };
+        
+        _i += 2;
+
+        let _class = match (bytestream[_i] as u16) << 8 | bytestream[_i + 1] as u16 {
+            1 => CLASS::IN,
+            _ => CLASS::UNKNOWN
+        };
+
+        Ok(DNSQuestion{
+            _name,
+            _type,
+            _class
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 mod dns;
 use dns::header::DNSHeader;
 
+use crate::dns::question::DNSQuestion;
+
 fn main(){
-    let _listener = std::net::UdpSocket::bind("0.0.0.0:5514").unwrap();
+    let _listener = std::net::UdpSocket::bind("0.0.0.0:53").unwrap();
     // listen and receive. 
     loop {
         let mut _buffer = [0u8; 1024];
         let (_count, _remoteaddr) = _listener.recv_from(&mut _buffer).unwrap();
-        println!("Received {:?} from {}", &_buffer[0.._count], _remoteaddr);
+        // println!("Received {:?} from {}", &_buffer[0.._count], _remoteaddr);
 
         // Get a DNS Header outta that 
-        let _newheader = DNSHeader::new(&_buffer[0.._count]);
+        let _newheader = DNSHeader::new(&_buffer[0.._count]).unwrap();
         println!("DNS Header read as: {:?}", _newheader);
+
+        // Try to read DNS question as well :D
+        let _newquestion = DNSQuestion::new(&_buffer[12.._count]).unwrap();
+        println!("DNS Question read as: {:?}", _newquestion);
+        println!("");
     }
 }
